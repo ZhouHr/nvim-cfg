3d5669c0032997d4fe123dd2b65ae6ee508c80f2
ZhouHr <zhr607@gmail.com>
2024-04-26 11:13:58 +0800

    init config

---
+++ lua/colorscheme.lua
@@ -0,0 +1,10 @@
+-- define your colorscheme here
+-- local colorscheme = 'monokai_pro'
+-- local colorscheme = 'nightfox'
+local colorscheme = 'dayfox'
+
+local is_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not is_ok then
+    vim.notify('colorscheme ' .. colorscheme .. ' not found!')
+    return
+end
